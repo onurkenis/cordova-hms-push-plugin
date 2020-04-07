be1915a09391427ae4b86e17c59a80ec99ee401a
onurkenis <kenisonur@yandex.com>
2020-04-07 11:07:35 +0300

    feat: dispatch onNotificationOpened event

--- www/CordovaHMSPushPlugin.js
+++ www/CordovaHMSPushPlugin.js
@@ -4,9 +4,21 @@ exports.getToken = function (arg0, success, error) {
     exec(success, error, "CordovaHMSPushPlugin", "getToken", [arg0]);
 }
 
-function getIntentData() {}
+function getIntentData() {
+	exec(null, null, "CordovaHMSPushPlugin", "getIntentData", []);
+}
 
-function setPushEventDispatcher() {}
+function setPushEventDispatcher() {
+	const intentData = getIntentData();
+    console.log("intentData", intentData);
+    
+	const json = JSON.parse(intentData);
+		
+	if(json.isNotification) {
+		// Dispatch the event.
+		elem.dispatchEvent(new CustomEvent('onNotificationOpened', json));
+	}
+}
 
 document.addEventListener("resume", setPushEventDispatcher, false);
 
